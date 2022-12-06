29ca68e0fe0196dc4fcd84206dd17d8e3087002d
Estate Lelashvili <estatelelashvili@gmail.com>
2022-12-06 19:38:10 +0400

    feat: implement donate burger menu popup

--- pages/donate/script.js
+++ pages/donate/script.js
@@ -51,3 +51,36 @@ document
       }
     }
   });
+
+///////////////////////////////
+// BURGER MENU
+
+function toggleBurgerMenu() {
+  document.getElementById('open-burger-menu-container-id').style.display =
+    'none';
+  const page = document.getElementsByTagName('body')[0];
+  page.classList.remove('noscroll');
+  document.getElementById('overlay-id').style.display = 'none';
+  // document.getElementById('overlay-id').style.zIndex = 1001;
+  document.getElementById('overlay-id').style.backgroundColor =
+    'rgba(0, 0, 0, 0)';
+}
+document.body.addEventListener('click', toggleBurgerMenu, true);
+document.getElementById('burger-control-id').onclick = function () {
+  document.getElementById('open-burger-menu-container-id').style.display =
+    'block';
+  document.getElementById('open-burger-menu-container-id').style.position =
+    'fixed';
+  document.getElementById('open-burger-menu-container-id').style.zIndex = 1001;
+  document.getElementById('overlay-id').style.display = 'block';
+  document.getElementById('overlay-id').style.backgroundColor =
+    'rgba(0, 0, 0, 0.3)';
+  const page = document.getElementsByTagName('body')[0];
+  page.classList.add('noscroll');
+};
+
+document.getElementById('burger-btn-menu-id').onclick = function (e) {
+  e.stopPropagation();
+  document.getElementById('open-burger-menu-container-id').style.display =
+    'none';
+};
